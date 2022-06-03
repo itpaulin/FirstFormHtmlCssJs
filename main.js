a153d651e2f15619274b1904c4735e98bb84ae06
Paulo Ricardo <prmaltaleal@gmail.com>
2022-06-02 22:30:32 -0300

    script defer comando novo

--- main.js
+++ main.js
@@ -2,26 +2,21 @@ const nameInput = document.querySelector("#name");
 const emailInput = document.querySelector("#email");
 const submitButton = document.querySelector("#submit-button");
 const errorMessage = document.querySelector(".msg");
-
-
-
+const items = document.querySelector(".items");
 submitButton.addEventListener("click", (e) => {
     e.preventDefault();
-
 
     const nameValue = nameInput.value;
     const emailValue = emailInput.value;
 
-    if (nameValue == '' || emailValue == '') {
-        errorMessage.textContent = "Please fill out the fields!";
+    if (nameValue == "" || emailValue == "") {
+        errorMessage.textContent = "Preencha todos os dados por favor";
         errorMessage.classList = "error";
 
         setTimeout(() => {
             errorMessage.textContent = "";
             errorMessage.classList = "";
-
         }, 3000);
         return;
     }
-    console.log(nameValue, emailValue)
 })
